3280037f7604ece490c837c1496b1c772611568b
Russian <russian.reboucas@gmail.com>
2016-04-28 15:53:42 -0300

    refactor(detectDevice): remove iife

--- src/detectDevice.js
+++ src/detectDevice.js
@@ -1,15 +1,13 @@
-(function () {
-  if (Framework7.prototype.device.android) {
-    Dom7('head').append(
-      '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.material.min.css">' +
-      '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.material.colors.min.css">'
-    );
-  }
-  else {
-    Dom7('.view').append('<div class="navbar"></div>')
-    Dom7('head').append(
-      '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.ios.min.css">' +
-      '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.ios.colors.min.css">'
-    );
-  }
-})();
+if (Framework7.prototype.device.android) {
+  Dom7('head').append(
+    '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.material.min.css">' +
+    '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.material.colors.min.css">'
+  );
+}
+else {
+  Dom7('.view').append('<div class="navbar"></div>')
+  Dom7('head').append(
+    '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.ios.min.css">' +
+    '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.ios.colors.min.css">'
+  );
+}
