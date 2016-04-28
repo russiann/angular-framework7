13d03c2c28a7f3706132f492254585bec9fe3e57
Russian <russian.reboucas@gmail.com>
2016-04-28 13:33:19 -0300

    feat(example): update example

--- example/app.js
+++ example/app.js
@@ -3,13 +3,11 @@ angular.module('app', ['framework7', 'pages'])
   .config(function($F7Provider, $F7RouterProvider){
 
     $F7Provider.init({
-      angular: true,
-      material: true,
       swipePanel: 'left'
     }, true);
 
     $F7Provider.addView('main', '.view-main', {
-      dynamicNavbar: false
+      dynamicNavbar: true
     }, true);
 
   })
